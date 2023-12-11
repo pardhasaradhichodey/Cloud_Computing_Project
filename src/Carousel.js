2da06a58f2b5bcea0d19c34b7e6f0cf31efc228a
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2023-12-11 08:48:56 -0800

    image error bug fix1

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,22 +1,18 @@
 // Carousel.js
 
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'; // Make sure to install 'react-bootstrap' package
+import React from "react";
+import Carousel from "react-bootstrap/Carousel"; // Make sure to install 'react-bootstrap' package
 
 //import image2 from './images/images1.jpeg';
 const PhotoCarousel = () => {
-    const carouselItemStyle = {
-        height: '430px', // Adjust the height as needed
-      };
-      const carouselInterval = 2000;
-    return (
+  const carouselItemStyle = {
+    height: "430px", // Adjust the height as needed
+  };
+  const carouselInterval = 2000;
+  return (
     <Carousel interval={carouselInterval}>
       <Carousel.Item style={carouselItemStyle}>
-        <img
-          className="d-block w-100"
-          src="/images/images.jpeg"
-          alt="First slide"
-        />
+        <img className="d-block w-100" src="/images.jpeg" alt="First slide" />
         <Carousel.Caption>
           <h3>Explore Your Community</h3>
           <p>Discover and report civic issues in your neighborhood.</p>
@@ -24,11 +20,7 @@ const PhotoCarousel = () => {
       </Carousel.Item>
 
       <Carousel.Item style={carouselItemStyle}>
-        <img
-          className="d-block w-100"
-          src="/images/images1.jpeg"
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src="/images1.jpeg" alt="Second slide" />
         <Carousel.Caption>
           <h3>Make a Difference</h3>
           <p>Contribute to the improvement of your local environment.</p>
